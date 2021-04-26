9850a13282be23396d13e0516dc6fda9e3bdd8b0
Icefoxzhx <1104161801@qq.com>
2021-04-26 09:37:28 +0800

    update codegen.bash for optimization

--- codegen.bash
+++ codegen.bash
@@ -1,3 +1,3 @@
 set ff=UNIX
 set -e
-cat | java -cp /ulib/java/antlr-4.9-complete.jar:./bin Main -codegen > output.s
+cat | java -cp /ulib/java/antlr-4.9-complete.jar:./bin Main -codegen -O2 > output.s
